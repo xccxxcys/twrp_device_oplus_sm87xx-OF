ffdf98a2f8af95803c87e2dc25284aa0855cb2b1
Nanya <xccx_2023@hotmail.com>
2025-12-22 21:14:39 +0800

    Fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ BUILD_BROKEN_PLUGIN_VALIDATION      := soong-libaosprecovery_defaults soong-libg
 TARGET_ARCH                 := arm64
 TARGET_ARCH_VARIANT         := armv8-a
 TARGET_CPU_ABI              := arm64-v8a
-TARGET_CPU_VARIANT          := kryo
+TARGET_CPU_VARIANT          := oryon
 
 # A/B
 AB_OTA_PARTITIONS := \
